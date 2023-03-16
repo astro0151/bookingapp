fee19b3e0a4bc1c98940b5ca96bee2224b24556f
astro0151 <gulbahar0151@gmail.com>
2023-03-17 02:06:25 +0530

    second commit;

--- src/main/java/com/sagarpathlab/registermodule/controller/HomeController.java
+++ src/main/java/com/sagarpathlab/registermodule/controller/HomeController.java
@@ -1,17 +1,26 @@
 package com.sagarpathlab.registermodule.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sagarpathlab.registermodule.entity.CustomerDetails;
 import com.sagarpathlab.registermodule.repository.CustomerRepository;
 
 import jakarta.servlet.http.HttpSession;
 
+
 @Controller
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/customers")
 public class HomeController {
 	@Autowired
 	private CustomerRepository custRepo;
@@ -21,6 +30,7 @@ public class HomeController {
 		return "index";
 	}
 	
+	
 	@PostMapping("/booking")
 	public String bookTest(@ModelAttribute CustomerDetails cd, HttpSession session ) {
 		System.out.println(cd);
@@ -28,6 +38,12 @@ public class HomeController {
 		custRepo.save(cd);
 		
 		return "redirect:/ ";
+	}
+//	code start for  fetching data from reactui
+	
+	@GetMapping
+	public List<CustomerDetails> getCustomerDetails(){
+		return custRepo.findAll();
 	}
 	
 }
